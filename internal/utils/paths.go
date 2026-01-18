7781efc06fa74f27b4d073e04e6a343c7efc3af9
Safidy-Mahefa <safidymahefa03@gmail.com>
2026-01-18 17:48:13 +0200

    initialize CLI

--- internal/utils/paths.go
+++ internal/utils/paths.go
@@ -1,1 +1,5 @@
 package utils
+
+// Fonction pour parser la commande
+// Fonction pour verifier si un texte est inclus dans la commande 
+// veridier si la commande est valide
--- main.go
+++ main.go
@@ -1,12 +1,28 @@
 package main
 
-import(
+import (
 	"fmt"
-	"GSF/internal/ghost"
-	"GSF/internal/metadata"
-	"GSF/internal/utils"
+	"os"
+	// "GSF/internal/ghost"
+	// "GSF/internal/metadata"
+	// "GSF/internal/utils"
 )
 
 func main() {
-	fmt.Printf("OK")
+	boucle:= true
+	commande := ""
+	for boucle{ //boucle pour faire tourner le CLI
+		fmt.Printf("-> ")
+		v , err := fmt.Scanf("%s",&commande) //entréé utilisateur
+		fmt.Println(v)
+		fmt.Printf("\n")
+
+		if err != nil { fmt.Println("Erreur :", err)}
+
+		if commande == "q"{
+			boucle = false
+		}else if commande == "r"{
+			os.Remove("file.txt")
+		}
+	}
 }
