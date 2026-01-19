a2b439cf479241abc816c459105ea81b2684d0bf
Safidy-Mahefa <safidymahefa03@gmail.com>
2026-01-19 17:49:13 +0200

    Finished delete fonct

--- internal/ghost/delete.go
+++ internal/ghost/delete.go
@@ -1,1 +1,18 @@
 package ghost
+ import(
+	"fmt"
+	"os"
+ )
+/*Quand un fichier est supprimé, il est déplacé dans le dossier caché ./ghost et ses métadonnés sont stockés dans un json */
+func Delete(file string) error{
+		// récuperer Les métadonnes du fichier
+	info,err:= os.Stat(file)
+	filename:= info.Name()
+
+	ghostDir := "./.ghost/"
+	dest := ghostDir + filename +".deleted" //la destination ou le fichier va etre deplace avec son nouveau nom
+
+	err = os.Rename(file,dest) //Déplacer le fichier et le renommer
+	if err != nil {fmt.Println(err)}
+	return err
+}
--- main.go
+++ main.go
@@ -1,28 +1,37 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
-	// "GSF/internal/ghost"
+	"strings"
+	"GSF/internal/ghost"
 	// "GSF/internal/metadata"
 	// "GSF/internal/utils"
 )
 
 func main() {
 	boucle:= true
-	commande := ""
+	reader := bufio.NewReader(os.Stdin) //pour lire l'entrée utilisateur à pusieurs espaces
+	var parts[]string // Le tableau qui contient les parts de la commande
+
 	for boucle{ //boucle pour faire tourner le CLI
+		// Input utilisateur
 		fmt.Printf("-> ")
-		v , err := fmt.Scanf("%s",&commande) //entréé utilisateur
-		fmt.Println(v)
+		commande , err := reader.ReadString('\n') //entréé utilisateur
 		fmt.Printf("\n")
+
+		parts = strings.Split(commande," ")
 
 		if err != nil { fmt.Println("Erreur :", err)}
 
+		// Gestion des commandes
 		if commande == "q"{
 			boucle = false
-		}else if commande == "r"{
-			os.Remove("file.txt")
+		}else if strings.Contains(commande,"remove") && parts[0] == "remove" { //si la commande contient "remove" et commence par remove,
+			file:=strings.TrimSpace(parts[1]) //nettoie le nom du fichier
+			fmt.Println(file)
+			ghost.Delete(file)
 		}
 	}
 }
